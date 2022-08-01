5723d738bd536367fbacd0bdbbc6ff10cbd8ab77
Douglas Henrique <douggx10@Hotmail.com>
2022-08-01 10:43:39 -0300

    organização de pastas

---
+++ rappi4/src/Routes/Coordinator.js
@@ -0,0 +1,27 @@
+export const goToDefault = (navigate) => {
+    navigate('/default')
+}
+
+export const goToLogin = (navigate) => {
+    navigate('/login')
+}
+
+export const goToSignup = (navigate) => {
+    navigate('/cadastro')
+}
+
+export const goToAdressSignup = (navigate) => {
+    navigate('/cadastro/endereco')
+}
+
+export const goToFeed = (navigate) => {
+    navigate('/feed')
+}
+
+export const goToRestaurants = (navigate) => {
+    navigate('/restaurantes')
+}
+
+export const goToOrderInProgress = (navigate) => {
+    navigate('/pedidoEmAndamento')
+}
